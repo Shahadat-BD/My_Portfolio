50525f23b3cf08946a91abac985588be04cfc560
Shahadat-BD <shahadatstudent34@gmail.com>
2023-12-13 11:39:20 +0600

    navbar design update for mobile user

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -9,18 +9,18 @@ const Banner = () => {
              <Navbar/>
             <div className='banner-design'>
                 <div className='lg:px-16 px-5'>
-                    <div className='flex lg:flex-row flex-col gap-5 justify-items-center items-center h-[540px]'>
-                        <div className='w-4/5'>
-                            <h3 className='text-5xl font-bold text-white'>Hi <br />I'am Shahadat hossain </h3>
-                            <h4 className='text-white my-3 font-bold text-4xl'><span className='text-[#DF5586]'>Front-end web developer</span></h4>
-                            <p className='lg:w-1/2 text-white'>Aspiring front-end developer evolving into a skilled full-stack developer,crafting seamless digital experiences. Passionate about coding and continuous learning</p>
+                    <div className='flex lg:flex-row flex-col-reverse gap-5 items-center lg:h-[640px] h-full'>
+                        <div className='lg:w-4/5 lg:mb-0 mb-10'>
+                            <h3 className='lg:text-5xl text-3xl font-bold text-white'>Hi <br />I'am Shahadat hossain </h3>
+                            <h4 className='text-white my-3 font-bold lg:text-4xl text-2xl'><span className='text-[#DF5586]'>Front-end web developer</span></h4>
+                            <p className='lg:w-1/2 w-full text-white'>Aspiring front-end developer evolving into a skilled full-stack developer,crafting seamless digital experiences. Passionate about coding and continuous learning</p>
                             <button className='hover:bg-[#DF5586] border border-white mt-5 text-white rounded-md px-7 py-3 font-bold text-lg mr-5'>Hire Me</button>
                             <a href={resume} download="Shahadat's_Resume_for_Front-end_Web_Developer.pdf">
                                    <button className='bg-[#DF5586] mt-5 text-white rounded-md px-7 py-3 font-bold text-lg'>Download CV</button>
                              </a>
                         </div>
                         <div>
-                              <img className='rounded-3xl' src={myImage} alt="" srcset="" />
+                              <img className='rounded-3xl w-full lg:mt-0 mt-20' src={myImage} alt="" srcset="" />
                         </div>
                     </div>
                 </div>
--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -2,23 +2,26 @@ import React from 'react';
 import logo from '../../assets/logo.png'
 const Navbar = () => {
   const link = <>
-          <li className='ml-8 lg:text-white  font-semibold text-lg'>Home</li>
-          <li className='ml-8 lg:text-white  font-semibold text-lg'>About</li>
-          <li className='ml-8 lg:text-white  font-semibold text-lg'>Contact</li>
-          <li className='ml-8 lg:text-white  font-semibold text-lg'>Project</li>
-          <li className='ml-8 lg:text-white  font-semibold text-lg'>Education</li>
+          <li className='lg:ml-8 mb-4 lg:text-left text-center lg:text-white  lg:bg-black bg-blue-500 rounded-md py-1 lg:py-0  font-semibold text-lg'>Home</li>
+          <li className='lg:ml-8 mb-4 lg:text-left text-center lg:text-white  lg:bg-black bg-blue-500 rounded-md py-1 lg:py-0  font-semibold text-lg'>About</li>
+          <li className='lg:ml-8 mb-4 lg:text-left text-center lg:text-white  lg:bg-black bg-blue-500 rounded-md py-1 lg:py-0  font-semibold text-lg'>Contact</li>
+          <li className='lg:ml-8 mb-4 lg:text-left text-center lg:text-white  lg:bg-black bg-blue-500 rounded-md py-1 lg:py-0  font-semibold text-lg'>Project</li>
+          <li className='lg:ml-8 mb-4 lg:text-left text-center lg:text-white  lg:bg-black bg-blue-500 rounded-md py-1 lg:py-0  font-semibold text-lg'>Education</li>
    </>
     return (
-        <div className="navbar bg-black lg:px-16 px-5">
+        <div className="navbar bg-black lg:px-16 px-5 fixed z-20">
         <div className="navbar-start">
           <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden text-white">
+            <div tabIndex={0} role="button" className="btn lg:hidden text-black bg-white">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </div>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            <ul tabIndex={0} className="menu menu-sm cursor-pointer dropdown-content mt-3 p-3 bg-[#24273E] text-white rounded-lg w-[330px]">
                {link}
             </ul>
           </div>
+          <img className='w-36 lg:block hidden' src={logo} alt="" srcset="" />
+        </div>
+        <div className='lg:hidden navbar-end'>
            <img className='w-36' src={logo} alt="" srcset="" />
         </div>
         <div className="navbar-end hidden lg:flex">
@@ -26,9 +29,6 @@ const Navbar = () => {
             {link}
           </ul>
         </div>
-        {/* <div className="navbar-end ">
-          <a className="btn">Button</a>
-        </div> */}
       </div>
     );
 };
