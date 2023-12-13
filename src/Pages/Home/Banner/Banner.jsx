ef166167367101853eba1784b56cceea106a08c8
Shahadat-BD <shahadatstudent34@gmail.com>
2023-12-13 10:14:09 +0600

    resume added in donwloadCv buttton of banner section

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import myImage from '../../../assets/images/shahadat.png'
 import './Banner.css'
 import Navbar from '../Navbar';
+import resume from "./Shahadat's_Resume_for_Front-end_Web_Developer.pdf"
 const Banner = () => {
     return (
         <div className=''> 
@@ -10,10 +11,13 @@ const Banner = () => {
                 <div className='lg:px-16 px-5'>
                     <div className='flex lg:flex-row flex-col gap-5 justify-items-center items-center h-[540px]'>
                         <div className='w-4/5'>
-                            <h3 className='text-5xl font-bold text-white'>I am shahadat hossain </h3>
-                            <h4 className='text-white my-3 font-bold text-xl'>I am front end web developer</h4>
+                            <h3 className='text-5xl font-bold text-white'>Hi <br />I'am Shahadat hossain </h3>
+                            <h4 className='text-white my-3 font-bold text-4xl'><span className='text-[#DF5586]'>Front-end web developer</span></h4>
                             <p className='lg:w-1/2 text-white'>Aspiring front-end developer evolving into a skilled full-stack developer,crafting seamless digital experiences. Passionate about coding and continuous learning</p>
-                            <button className='bg-red-500 mt-5 text-white rounded-md px-8 py-3'>Download CV</button>
+                            <button className='hover:bg-[#DF5586] border border-white mt-5 text-white rounded-md px-7 py-3 font-bold text-lg mr-5'>Hire Me</button>
+                            <a href={resume} download="Shahadat's_Resume_for_Front-end_Web_Developer.pdf">
+                                   <button className='bg-[#DF5586] mt-5 text-white rounded-md px-7 py-3 font-bold text-lg'>Download CV</button>
+                             </a>
                         </div>
                         <div>
                               <img className='rounded-3xl' src={myImage} alt="" srcset="" />
